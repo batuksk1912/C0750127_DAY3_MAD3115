617a9f6b2a1f509aba0a84029a8dfb923ff010d4
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-06 18:14:29 -0500

    Initial Commit

---
+++ C0750127_DAY3_MAD3115/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0750127_DAY3_MAD3115
+//
+//  Created by Baturay Kayatürk on 6.03.2019.
+//  Copyright © 2019 Lambton College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
